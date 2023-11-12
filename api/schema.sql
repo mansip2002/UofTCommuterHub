b9c71fbde578388137347daa9d5fe624fd120b2a
mansip2002 <mansi2156@gmail.com>
2023-11-12 11:13:19 -0500

    Added table for user commuting data

--- api/schema.sql
+++ api/schema.sql
@@ -1,7 +1,18 @@
 CREATE TABLE account (
     id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
     email VARCHAR(255),
     password_hash VARCHAR(255)
 )
+
+CREATE TABLE user_profile (
+    user_id uuid,
+    name VARCHAR(255) NOT NULL,
+    start_location VARCHAR(255) NOT NULL,
+    end_location VARCHAR(255) NOT NULL,
+    account_id uuid REFERENCES account(id),
+    day_of_week VARCHAR(10) NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL, 
+    PRIMARY KEY (user_id, day_of_week, start_location, end_location),
+    FOREIGN KEY (user_id) REFERENCES account(id)
+);
