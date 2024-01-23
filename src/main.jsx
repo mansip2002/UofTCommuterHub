54e6c37cc7df7e519713e1bd26af6d4346479d52
mansip2002 <70120613+mansip2002@users.noreply.github.com>
2024-01-23 17:43:51 -0500

    Update main.jsx original version, modified to not use jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,6 @@ import './index.css'
 import { createRoot } from 'react-dom/client'
 
 const root = document.getElementById('root');
-const hello = React.createElement('div', null, 'Hello!');
-ReactDOM.createRoot(root).render(hello);
+const app = React.createElement(App);
+const strictMode = React.createElement(React.StrictMode, null, app);
+ReactDOM.createRoot(root).render(strictMode);
