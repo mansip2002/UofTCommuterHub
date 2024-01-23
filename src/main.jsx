fc1591b68ef55e0c6ec6d21869f6242664959bbf
mansip2002 <70120613+mansip2002@users.noreply.github.com>
2024-01-23 17:26:21 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
+import { createRoot } from 'react-dom/client'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<div>Hello, World!</div>);
