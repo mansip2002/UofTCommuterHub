0861b262450deb7c35007b642c226eed0a3f5c68
krishnapatel17 <74744575+krishnapatel17@users.noreply.github.com>
2024-01-23 22:31:44 -0500

    Update Login.jsx to change url to production flask url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
   }, [params]);
 
   const onSubmit = async () => {
-    const response = await fetch("http://127.0.0.1:5000/login", {
+    const response = await fetch("https://uoftcommuterhubbackend-dntd.onrender.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
